ce36ee55e289cd23d2f30dae529ba28833fa9b95
Keita SAITOU <Keita SAITOU@localhost>
2013-03-23 20:03:57 +0900

    fixed variable type to volatile

--- c_pth/hello100.c
+++ c_pth/hello100.c
@@ -6,12 +6,19 @@ void *hello(void*);
 
 int main(int argc, char** argv)
 {
-	pthread_t pt;
+	int n = 50;
+	pthread_t (*pt)[n];
 	int i;
 
-	for(i=0; i<100; i++){
-		pthread_create(&pt, NULL, &hello, (void*) &i);
-		sleep(0.1);
+	for(i=0; i<n; i++){
+/*
+int  pthread_create(pthread_t  *  thread,
+                    pthread_attr_t * attr,
+                    void * (*start_routine)(void *),
+                    void * arg);
+ */
+		pthread_create(pt[i], NULL, &hello, (void*) &i);
+		sleep(1);
 	}
 	sleep(60);
 	return 0;
@@ -19,15 +26,15 @@ int main(int argc, char** argv)
 
 void *hello(void* vp)
 {
-	char* mess = "Hello\n";
 	int *thread_num = (int*)vp;
-	int i;
+	volatile int i;
 
 	for(i=0; i<10; i++){
+		/* TODO: flash printf */
 		printf("Hello, I'm thread %d, and I've say %dth.\n",
-		       *thread_num, i);
-		sleep(0.2);
+		       *thread_num,
+		       i);
+		sleep(2);
 	}
-	return 0;
 }
 
